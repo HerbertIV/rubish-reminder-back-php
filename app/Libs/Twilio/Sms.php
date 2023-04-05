d3d5dc544fdbc0e6403abfaf5389885dcdac6a15
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2023-04-05 23:40:42 +0200

    Add twilio integration

---
+++ app/Libs/Twilio/Sms.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Libs\Twilio;
+
+use Illuminate\Support\Facades\Log;
+use Twilio\Exceptions\TwilioException;
+
+class Sms extends Twilio
+{
+    public function send(string $to, string $message)
+    {
+        try {
+            $this->client->messages->create(
+                $to,
+                [
+                    "body" => $message,
+                    "from" => env('TWILIO_NUMBER')
+                    //   On US phone numbers, you could send an image as well!
+                    //  'mediaUrl' => $imageUrl
+                ]
+            );
+            Log::channel('sms')->info('Message sent to ' . $to);
+        } catch (TwilioException $e) {
+            Log::error(
+                'Could not send SMS notification.' .
+                ' Twilio replied with: ' . $e
+            );
+        }
+    }
+}
